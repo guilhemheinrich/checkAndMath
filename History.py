be35b93dc90d29c6b1127a0ecd08d0bc033c6709
guilhemheinrich <guilghem.heinrich@gmail.com>
2018-01-07 21:24:31 +0100

    TODO reorder method in History

--- History.py
+++ History.py
@@ -3,14 +3,14 @@ import Operator
 
 
 class History:
+    symetric_operators = []
+    inverse_operators = {}
     def __init__(self, **kwargs):
         self.tab_operators = []
         self.tab_letters = [0]
-        self.symetric_operators = []
         if kwargs.has_key('history'):
             self.tab_operators = copy.copy(kwargs['history'].tab_operators)
             self.tab_letters = copy.copy(kwargs['history'].tab_letters)
-            self.symetric_operators = kwargs['history'].symetric_operators
     def addItem(self, operator, value):
         self.tab_operators.append(operator)
         self.tab_letters.append(value)
@@ -24,11 +24,18 @@ class History:
         string_tab = []
         last_operator = self.tab_operators[0]
         orderedLetters = [self.tab_letters[0]]
+        orderedOperatorsName = []
         for index in range(self.tab_operators.__len__()):
             operator = self.tab_operators[index]
-            if last_operator == operator:
+            # Check if the opertor is symetric
+            if (last_operator == operator) and (operator in History.symetric_operators):
                 orderedLetters.append(self.tab_letters[index + 1])
+                orderedOperatorsName.append(operator.name)
+            elif last_operator in History.symetric_operators and History.symetric_operators[last_operator] == operator:
+                orderedLetters.append(self.tab_letters[index + 1])
+                orderedOperatorsName.append(operator.name)
             else:
+                self.reorder(orderedLetters, orderedOperatorsName)
                 ol_size = orderedLetters.__len__()
                 if ol_size > 1:
                     orderedLetters.sort()
@@ -46,7 +53,18 @@ class History:
         for subindex in range(ol_size - 1):
             string_tab.append(last_operator.name)
             string_tab.append(str(orderedLetters[subindex + 1]))
+        self.reorder(orderedLetters, orderedOperatorsName)
         return '_'.join(string_tab)
+    '''
+    Function to reorder the letters (values) / operation 
+    If there is an inverse function, both components should be sorted and the merged
+    '''
+    def reorder(self, orderedLetters, orderedOperatorsName):
+        pass
 
 
 
+# orderedOperatorsName = ['+', '+', '-', '+', '+', '-']
+# orderedLetters = [2, 5, 7, 11, 3, 13, 9]
+
+# sorted( range(len(orderedOperatorsName)), key = lambda k : orderedOperatorsName[k])
--- chess.py
+++ chess.py
@@ -3,15 +3,17 @@ import Operator
 import History
 
 # teamA = [2,3,5,7,9,11,17,19,23]
-teamA = [2,3,5,7,9,11]
+teamA = [2,3,5,7]
 teamB = [2,3,5,7,9,11,17,19,23]
 
 allOperator = (Operator.operatorAdd, Operator.operatorMinus, Operator.operatorMultiply, Operator.operatorDivide)
 symetricOperator = (Operator.operatorAdd, Operator.operatorMinus, Operator.operatorMultiply)
+inverseOperator ={ Operator.operatorAdd : Operator.operatorMinus, Operator.operatorMinus : Operator.operatorAdd }
 
 root = SolutionBranch.SolutionBranch(alphabet = teamA, targets_value = teamB)
-
+# root.history.symetric_operators = symetricOperator
 # root.generate(0, Operator.operatorAdd)
+# History.History.symetric_operators = symetricOperator
 
 allSolution = {}
 for key in teamB:
@@ -23,6 +25,10 @@ allSolution[11][2][0].history.getKey()
 
 for index, key in enumerate(allSolution[11][2]):
     print((index, key))
+    print(allSolution[11][2][key].history.symetric_operators)
+
+for sol in allSolution[11][2]:
+    print(sol.history.symetric_operators)
 
 
 def showDepth(depth):
